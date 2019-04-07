70215d8d49e4e55063d003139ba4b01f2365a1d2
DelsinPotter <49351795+DelsinPotter@users.noreply.github.com>
2019-04-07 22:52:20 +0500

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,11 @@
+class Task2{
+public static void main(String[] args){
+double y,num=50.25;
+int x;
+x=(int)num;
+y=num-x;
+System.out.println("Original="+num);
+System.out.println("Integer part="+x);
+System.out.println("Decimal part="+y);
+}
+}
