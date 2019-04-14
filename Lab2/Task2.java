4b97542db2436d997468c42ced2803c4141039fa
DelsinPotter <49351795+DelsinPotter@users.noreply.github.com>
2019-04-14 16:29:05 +0500

    Create Task2.java

---
+++ Lab2/Task2.java
@@ -0,0 +1,9 @@
+class Task2{
+			public static void main(String[] args){
+			int a=2,b=3;
+			boolean c1=a<b,c2=a>b,c3=(a<=b);   //c1 is true, c2 is false, c3 is true
+			System.out.println(c1+" "+c2+" "+c3); 
+			System.out.println((c1&&c2)+" "+(c2||c3)+" "+(!c3));
+			
+			}
+}
