153b0c5efd8dfd426dac54f19343a028367f929f
DelsinPotter <49351795+DelsinPotter@users.noreply.github.com>
2019-04-14 16:31:43 +0500

    Create Task4.java

---
+++ Lab2/Task4.java
@@ -0,0 +1,12 @@
+class Task4{
+public static void main(String[] args){
+double pi=3.142,radius=30.5,circum,diameter,area;
+circum=2*pi*radius;
+diameter=2*radius;
+area=pi*radius*radius;
+System.out.println("circumference="+circum);
+System.out.println("diameter="+diameter);
+System.out.println("area="+area);
+
+}
+}
